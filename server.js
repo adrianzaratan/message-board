fe97201d0c9ce0a7612a4b2a8790b3c6207ba86d
adrianzaratan <adrianzaratan27@gmail.com>
2022-05-13 11:10:56 -1200

    put on github

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict';
 require('dotenv').config();
+
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+require('./db-connection');
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
-
+const helmet = require('helmet');
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +18,14 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet.frameguard());
+app.use(
+  helmet({
+    referrerPolicy: { policy: "same-origin" },
+  })
+);
+app.use(helmet.dnsPrefetchControl());
 
 //Sample front-end
 app.route('/b/:board/')
@@ -58,7 +68,7 @@ const listener = app.listen(process.env.PORT || 3000, function () {
         console.log('Tests are not valid:');
         console.error(e);
       }
-    }, 1500);
+    }, 10000);
   }
 });
 
